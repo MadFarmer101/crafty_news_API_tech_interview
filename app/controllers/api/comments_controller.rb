58810069b3bb41992562412729ac4be1042a11f7
Janko <mailodjanka@gmail.com>
2020-04-22 14:17:49 +0200

    Defines a private method in Comments Controlles to find article by id

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,12 @@
 class Api::CommentsController < ApplicationController
-  
+  before_action :find_article
+
   def create
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
