c5d0959e40ddefacc95f4908c1b66a97cefb6d63
Janko <mailodjanka@gmail.com>
2020-04-22 15:43:28 +0200

    Adds a spec to check for success message + adds the sad path spec for trying to post an empty comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,15 +1,21 @@
 class Api::CommentsController < ApplicationController
+  before_action :find_article
 
   def create
     comment = Comment.create(comment_params)
+    comment.article_id = params[:article_id]
     if comment.persisted?
       render json: { message: 'Your comment is saved.' }
     else
-      render json: { errors: comment.errors }, status: 422
+      render json: { error: "Body can't be blank." }, status: 422
     end
   end
 
   private
+
+  def find_article
+    @article = Article.find_by_id(params[:article_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:body, :article_id)
--- spec/requests/api/comment/user_can_create_a_comment_spec.rb
+++ spec/requests/api/comment/user_can_create_a_comment_spec.rb
@@ -15,5 +15,49 @@ RSpec.describe "POST /api/comments", type: :request do
     it "returns a 200 response" do
       expect(response).to have_http_status 200
     end
+
+    it "returns a success message" do
+      expect(response_json["message"]).to eq 'Your comment is saved.'
+    end
+  end
+
+  describe "unsuccessfully" do
+    before do
+      post "/api/comments",
+        params: {
+          comment: {
+            body: '',
+            article_id: article.id
+          } 
+        } 
+    end
+
+    it "returns a 422 response" do
+      expect(response).to have_http_status 422
+    end
+
+    it "returns an error message" do
+      expect(response_json["error"]).to eq "Body can't be blank."
+    end
+  end
+
+  describe "unsuccessfully" do
+    before do
+      post "/api/comments",
+        params: {
+          comment: {
+            body: '',
+            article_id: article.id
+          } 
+        } 
+    end
+
+    it "returns a 422 response" do
+      expect(response).to have_http_status 422
+    end
+
+    it "returns an error message" do
+      expect(response_json["error"]).to eq "Body can't be blank."
+    end
   end
 end
